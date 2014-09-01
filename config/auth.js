a1e3095158ba98eaf4fa3bb9fce8cfbd85a4b23b
absyah <ardian.bahtiarsyah@gmail.com>
2014-09-01 21:24:21 +0700

    deployment

--- config/auth.js
+++ config/auth.js
@@ -4,9 +4,9 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '374111722736499', // your App ID
-        'clientSecret'  : '75929c20793a1f8228d8141d463f38d3', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'clientID'      : '837558732934300', //'374111722736499', // your App ID
+        'clientSecret'  : '76034c7ede0e036ec2eb2c825d1cf797', // '75929c20793a1f8228d8141d463f38d3', // your App Secret
+        'callbackURL'   : 'http://exvex.nodejitsu.com/auth/facebook/callback' // 'http://localhost:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/exvex'
+    // 'url' : 'mongodb://localhost/exvex'
+    'url' : 'mongodb://nodejitsu:34b4cb89ad3dcfd0c02708b83aca85d1@troup.mongohq.com:10083/nodejitsudb8059876558' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
 
 };
